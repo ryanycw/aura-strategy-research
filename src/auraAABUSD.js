f25c9c1e3c1faf691bbffa0733f6663849e763fa
ryanycw <ryan0213.tem04@nctu.edu.tw>
2022-12-20 14:40:31 +0800

    fetch deposit balances

---
+++ src/auraAABUSD.js
@@ -0,0 +1,42 @@
+require("dotenv/config");
+const fs = require('fs');
+const cliProgress = require('cli-progress');
+const { ethers, Wallet } = require("ethers");
+const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_MAINNET_URL);
+const walletPrivateKey = new Wallet(process.env.PK1);
+const signer = walletPrivateKey.connect(provider);
+const result = {"balancesList":[]};
+const contractFile = require("../import/abi/BaseRewardPool4626.json");
+const depositList = require("../import/allOther-depositList.json")
+
+async function main() {
+    console.log("Making signatures with the account:", (await signer.getAddress()));
+    console.log("Account balance:", (await signer.getBalance()).toString());
+    
+    factory = new ethers.ContractFactory(contractFile.abi, contractFile.bytecode, signer);
+    reward = await factory.attach(process.env.AABUSD_REWARD_CONTRACT_MAINNET);
+    console.log("Token Address: ", reward.address);
+    const totalDepositListLen = depositList.record.length
+    
+    const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    bar1.start(totalDepositListLen,0);
+    for(let i=0; i<totalDepositListLen; i++) {
+        bar1.increment();
+        const depositBalance = await reward.balanceOf(depositList.record[i].address);
+        if (ethers.BigNumber.from(depositBalance).gt(ethers.BigNumber.from('0'))) {
+            const outputData = {address: depositList.record[i].address,
+                                totalBalance: ethers.utils.formatEther(ethers.BigNumber.from(depositBalance).toString())};
+            result["balancesList"].push(outputData);
+        }
+    }
+    bar1.stop();
+
+    fs.writeFileSync('./out/aurabb-a-USD-vault.json', JSON.stringify(result));
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+});
